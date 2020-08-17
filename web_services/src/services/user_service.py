0b0ce62147bf09f588f278a5611045b05f80fb5a
Enzo <e.radnai@hotmail.fr>
2020-08-17 12:25:21 +0200

    adding model and repo

--- web_services/src/services/user_service.py
+++ web_services/src/services/user_service.py
@@ -1,25 +1,51 @@
 from dto.user_dto import UserDto
+from dto.user_dto_full import UserDtoFull
+from models.user import User
+import repositories.user_repo as ur
 
 
 def get_users():
-    return []
+    result = ur.get_users()
+    users = []
+    for user in result:
+        users.append(model_to_dto(user))
+    return users
 
 
 def get_user_by_name(user_name):
-    return user_name
+    result = ur.get_user_by_name(user_name)
+    users = []
+    for user in result:
+        users.append(model_to_dto(user))
+    return users
 
 
 def create_user(json):
-    user_dto = UserDto(json['lastname'], json['firstname'],
-                       json['birth'], json['email'], json['login'], json['password'])
-    return user_dto
+    user_dto = json_to_dto(json)
+    result = ur.create_user(dto_to_model(user_dto))
+    user = model_to_dto(user)
+    return user
 
 
 def update_user(user_id, json):
-    user_dto = UserDto(json['lastname'], json['firstname'],
-                       json['birth'], json['email'], json['login'], json['password'])
-    return user_id
+    user_dto = json_to_dto(json)
+    result = ur.update_user(user_id, user_dto)
+    user = model_to_dto(user)
+    return user
 
 
 def delete_user(user_id):
-    return user_id
+    ur.delete_user(user_id)
+
+
+def json_to_dto(json):
+    return UserDto(json['lastname'], json['firstname'],
+                   json['birth'], json['email'], json['login'], json['password'])
+
+
+def model_to_dto(model):
+    return UserDtoFull(model.id, model.lastname, model.firstname, model.birth, model.email, model.login, model.password)
+
+
+def dto_to_model(dto):
+    return User(dto.lastname, dto.firstname, dto.birth, dto.email, dto.login, dto.password)
